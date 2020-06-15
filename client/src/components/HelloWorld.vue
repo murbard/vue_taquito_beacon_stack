e6743706d5f7b9d5339e69a37e4bdeca491e11bf
Arthur B <murbard@users.noreply.github.com>
2020-06-15 09:44:54 +0100

    initial commit

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="hello">
+    <Tezos/>
+    <h1>{{ msg }}</h1>
+    <button v-on:click="toggle">toggle</button>
+    <h2>Essential Links</h2>
+    <ul>
+      <li>
+        <a
+          href="https://vuejs.org"
+          target="_blank"
+        >
+          Core Docs
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://forum.vuejs.org"
+          target="_blank"
+        >
+          Forum
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://chat.vuejs.org"
+          target="_blank"
+        >
+          Community Chat
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/vuejs"
+          target="_blank"
+        >
+          Twitter
+        </a>
+      </li>
+      <br>
+      <li>
+        <a
+          href="http://vuejs-templates.github.io/webpack/"
+          target="_blank"
+        >
+          Docs for This Template
+        </a>
+      </li>
+    </ul>
+    <h2>Ecosystem</h2>
+    <ul>
+      <li>
+        <a
+          href="http://router.vuejs.org/"
+          target="_blank"
+        >
+          vue-router
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vuex.vuejs.org/"
+          target="_blank"
+        >
+          vuex
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://vue-loader.vuejs.org/"
+          target="_blank"
+        >
+          vue-loader
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Tezos from './Tezos.vue'
+export default {
+  name: 'HelloWorld',
+  components: {
+    Tezos
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+      tiggered: false
+    }
+  },
+  methods: {
+    toggle: function () {
+      if (this.tiggered) {
+        this.tiggered = false
+        this.msg = 'TIGGERED!'
+      } else {
+        this.tiggered = true
+        this.msg = 'UNTIGGERED!'
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
